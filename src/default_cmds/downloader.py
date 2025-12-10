8a1cfd3a7156529c098269c55394619d4cfd18ff
prxblms <malcondev.lds@hotmail.com>
2025-12-10 13:51:46 -0300

    new

--- src/default_cmds/downloader.py
+++ src/default_cmds/downloader.py
@@ -1,14 +1,14 @@
-import os
+import os, yt_dlp
 
 # Components
 from components.settings import TITLE, fg_text, fg_error
 from components.functions import input_cmd, alert
 
 def BANNER_TITLE():
-    return f"""
+    return f"""{fg_error}
         ┳┓┏┓┓ ┏┳┓┓ ┏┓┏┓┳┓┏┓┳┓
         ┃┃┃┃┃┃┃┃┃┃ ┃┃┣┫┃┃┣ ┣┫
-        ┻┛┗┛┗┻┛┛┗┗┛┗┛┛┗┻┛┗┛┛┗
+        ┻┛┗┛┗┻┛┛┗┗┛┗┛┛┗┻┛┗┛┛┗ ver. 1.0
     """
 
 class Downloader():
@@ -22,7 +22,19 @@ class Downloader():
             args = input_cmd()
             if not args:
                 alert('error', f"{fg_text}Nenhum comando foi digitado! Tente usar [ {fg_error}help {fg_text}].")
+
+            elif args[0] == 'exit':
+                alert('info', "Saindo...")
+                exit()
+
+            elif args[0] == 'clear':
+                os.system('cls')
+                print(BANNER_TITLE())
+
+            # Tratamento dos comandos de baixar música e vídeo
             
+            else:
+                alert('error', "Esse comando não existe!")
 
 if __name__ == '__main__':
     try:
